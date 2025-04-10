373b85b8297ada69380c73a29eccb48afc7c1cf8
Anderson Oliveira <andoliv@gmail.com>
2025-04-10 21:46:51 +1200

    Initial commit

---
+++ src/test/java/com/andoliv/weatherinformation/WeatherInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andoliv.weatherinformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherInformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
